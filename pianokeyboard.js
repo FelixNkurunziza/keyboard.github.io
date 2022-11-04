72f6f38680263d6b5041058c50c6f961b485307b
unknown <felixnkurunzizankf@yahoo.fr>
2022-11-04 23:33:22 +0100

    Added the javascript file

---
+++ pianokeyboard.js
@@ -0,0 +1,190 @@
+function playsound(audio) {
+    audio.load();
+    audio.play();
+}
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#C2').onclick = function() {
+        playsound(document.querySelector('#c2'))
+    };
+    document.querySelector('#D2').onclick = function() {
+        playsound(document.querySelector('#d2'))
+    };
+    document.querySelector('#Db2').onclick = function() {
+        playsound(document.querySelector('#db2'))
+    };
+    document.querySelector('#E2').onclick = function() {
+        playsound(document.querySelector('#e2'))
+    };
+    document.querySelector('#Eb2').onclick = function() {
+        playsound(document.querySelector('#eb2'))
+    };
+    document.querySelector('#F2').onclick = function() {
+        playsound(document.querySelector('#f2'))
+    };
+    document.querySelector('#G2').onclick = function() {
+        playsound(document.querySelector('#g2'))
+    };
+    document.querySelector('#Gb2').onclick = function() {
+        playsound(document.querySelector('#gb2'))
+    };
+    document.querySelector('#A2').onclick = function() {
+        playsound(document.querySelector('#a2'))
+    };
+    document.querySelector('#Ab2').onclick = function() {
+        playsound(document.querySelector('#ab2'))
+    };
+    document.querySelector('#B2').onclick = function() {
+        playsound(document.querySelector('#b2'))
+    };
+    document.querySelector('#Bb2').onclick = function() {
+        playsound(document.querySelector('#bb2'))
+    };
+    document.querySelector('#C3').onclick = function() {
+        playsound(document.querySelector('#c3'))
+    };
+    document.querySelector('#D3').onclick = function() {
+        playsound(document.querySelector('#d3'))
+    };
+    document.querySelector('#Db3').onclick = function() {
+        playsound(document.querySelector('#db3'))
+    };
+    document.querySelector('#E3').onclick = function() {
+        playsound(document.querySelector('#e3'))
+    };
+    document.querySelector('#Eb3').onclick = function() {
+        playsound(document.querySelector('#eb3'))
+    };
+    document.querySelector('#F3').onclick = function() {
+        playsound(document.querySelector('#f3'))
+    };
+    document.querySelector('#G3').onclick = function() {
+        playsound(document.querySelector('#g3'))
+    };
+    document.querySelector('#Gb3').onclick = function() {
+        playsound(document.querySelector('#gb3'))
+    };
+    document.querySelector('#A3').onclick = function() {
+        playsound(document.querySelector('#a3'))
+    };
+    document.querySelector('#Ab3').onclick = function() {
+        playsound(document.querySelector('#ab3'))
+    };
+    document.querySelector('#B3').onclick = function() {
+        playsound(document.querySelector('#b3'))
+    };
+    document.querySelector('#Bb3').onclick = function() {
+        playsound(document.querySelector('#bb3'))
+    };
+    document.querySelector('#C4').onclick = function() {
+        playsound(document.querySelector('#c4'))
+    };
+    document.querySelector('#D4').onclick = function() {
+        playsound(document.querySelector('#d4'))
+    };
+    document.querySelector('#Db4').onclick = function() {
+        playsound(document.querySelector('#db4'))
+    };
+    document.querySelector('#E4').onclick = function() {
+        playsound(document.querySelector('#e4'))
+    };
+    document.querySelector('#Eb4').onclick = function() {
+        playsound(document.querySelector('#eb4'))
+    };
+    document.querySelector('#F4').onclick = function() {
+        playsound(document.querySelector('#f4'))
+    };
+    document.querySelector('#G4').onclick = function() {
+        playsound(document.querySelector('#g4'))
+    };
+    document.querySelector('#Gb4').onclick = function() {
+        playsound(document.querySelector('#gb4'))
+    };
+    document.querySelector('#A4').onclick = function() {
+        playsound(document.querySelector('#a4'))
+    };
+    document.querySelector('#Ab4').onclick = function() {
+        playsound(document.querySelector('#ab4'))
+    };
+    document.querySelector('#B4').onclick = function() {
+        playsound(document.querySelector('#b4'))
+    };
+    document.querySelector('#Bb4').onclick = function() {
+        playsound(document.querySelector('#bb4'))
+    };
+    document.querySelector('#C5').onclick = function() {
+        playsound(document.querySelector('#c5'))
+    };
+    document.querySelector('#D5').onclick = function() {
+        playsound(document.querySelector('#d5'))
+    };
+    document.querySelector('#Db5').onclick = function() {
+        playsound(document.querySelector('#db5'))
+    };
+    document.querySelector('#E5').onclick = function() {
+        playsound(document.querySelector('#e5'))
+    };
+    document.querySelector('#Eb5').onclick = function() {
+        playsound(document.querySelector('#eb5'))
+    };
+    document.querySelector('#F5').onclick = function() {
+        playsound(document.querySelector('#f5'))
+    };
+    document.querySelector('#G5').onclick = function() {
+        playsound(document.querySelector('#g5'))
+    };
+    document.querySelector('#Gb5').onclick = function() {
+        playsound(document.querySelector('#gb5'))
+    };
+    document.querySelector('#A5').onclick = function() {
+        playsound(document.querySelector('#a5'))
+    };
+    document.querySelector('#Ab5').onclick = function() {
+        playsound(document.querySelector('#ab5'))
+    };
+    document.querySelector('#B5').onclick = function() {
+        playsound(document.querySelector('#b5'))
+    };
+    document.querySelector('#Bb5').onclick = function() {
+        playsound(document.querySelector('#bb5'))
+    };
+    document.querySelector('#C6').onclick = function() {
+        playsound(document.querySelector('#c6'))
+    };
+    document.querySelector('#D6').onclick = function() {
+        playsound(document.querySelector('#d6'))
+    };
+    document.querySelector('#Db6').onclick = function() {
+        playsound(document.querySelector('#db6'))
+    };
+    document.querySelector('#E6').onclick = function() {
+        playsound(document.querySelector('#e6'))
+    };
+    document.querySelector('#Eb6').onclick = function() {
+        playsound(document.querySelector('#eb6'))
+    };
+    document.querySelector('#F6').onclick = function() {
+        playsound(document.querySelector('#f6'))
+    };
+    document.querySelector('#G6').onclick = function() {
+        playsound(document.querySelector('#g6'))
+    };
+    document.querySelector('#Gb6').onclick = function() {
+        playsound(document.querySelector('#gb6'))
+    };
+    document.querySelector('#A6').onclick = function() {
+        playsound(document.querySelector('#a6'))
+    };
+    document.querySelector('#Ab6').onclick = function() {
+        playsound(document.querySelector('#ab6'))
+    };
+    document.querySelector('#B6').onclick = function() {
+        playsound(document.querySelector('#b6'))
+    };
+    document.querySelector('#Bb6').onclick = function() {
+        playsound(document.querySelector('#bb6'))
+    };
+    document.querySelector('#C7').onclick = function() {
+        playsound(document.querySelector('#c7'))
+    };
+  
+})
